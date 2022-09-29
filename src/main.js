3234f6e6fea7eca9947f83cdbf18cea172351918
Peter Trofimov <ptrofimov7@gmail.com>
2022-09-29 11:26:08 +0300

    adds main page

--- src/main.js
+++ src/main.js
@@ -3,5 +3,21 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'materialize-css/dist/js/materialize.min'
+import tooltip from './directives/tooltip'
+import dateTimeFormat from './filters/date-format'
+import currencyFormat from './filters/currency-format'
+import messagePlugin from './utils/message-plugin'
+import Loader from '@/components/app/Loader'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$filters = {
+   dateTimeFormat,
+   currencyFormat
+}
+app.component("Loader", Loader)
+app.directive('tooltip', tooltip)
+app.use(store).use(router).use(messagePlugin).mount('#app')
+
+
+
